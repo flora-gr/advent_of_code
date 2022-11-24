e5342fc23d03caf42d488acc52564570d0e0c9e6
Flora Groothuizen <floragroothuizen@gmail.com>
2022-11-24 21:18:23 +0100

    Set up empty project

---
+++ README.md
@@ -0,0 +1,1 @@
+Dart solutions to Advent of Code 2022
---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:advent_of_code/test.dart' as test;
+
+void main(List<String> arguments) {
+  print('Hello world: ${test.calculate()}!');
+}
