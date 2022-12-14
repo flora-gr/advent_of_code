f355b4bd4d49eabab0155369af31d9ab8f92c5c6
Flora Groothuizen <floragroothuizen@gmail.com>
2022-12-14 13:54:05 +0100

    Measure calculation times

--- lib/base/base.dart
+++ lib/base/base.dart
@@ -6,19 +6,38 @@ Future<void> calculate(int day) async {
   final List<String> input = await File('./input/day$day/input').readAsLines();
 
   dataCache = null;
+  final Stopwatch stopwatch = Stopwatch()..start();
   final Object firstExampleSolution = calculateFirst(example);
+  stopwatch.stop();
+  final int firstExampleTime = stopwatch.elapsedMilliseconds;
+  stopwatch
+    ..reset()
+    ..start();
   final Object secondExampleSolution = calculateSecond(example);
+  stopwatch.stop();
+  final int secondExampleTime = stopwatch.elapsedMilliseconds;
+
   dataCache = null;
+  stopwatch
+    ..reset()
+    ..start();
   final Object firstSolution = calculateFirst(input);
+  stopwatch.stop();
+  final int firstSolutionTime = stopwatch.elapsedMilliseconds;
+  stopwatch
+    ..reset()
+    ..start();
   final Object secondSolution = calculateSecond(input);
+  stopwatch.stop();
+  final int secondSolutionTime = stopwatch.elapsedMilliseconds;
 
   print('Solutions day $day:\n'
       'First part example: $firstExampleSolution, '
-      'which is ${_isCorrect(firstExampleSolution, exampleAnswerFirst) ? 'correct' : 'incorrect'}\n'
-      'First part solution: $firstSolution\n'
+      'which is ${_isCorrect(firstExampleSolution, exampleAnswerFirst) ? 'correct' : 'incorrect'} (${firstExampleTime}ms)\n'
+      'First part solution: $firstSolution (${firstSolutionTime}ms)\n'
       'Second part example: $secondExampleSolution, '
-      'which is ${_isCorrect(secondExampleSolution, exampleAnswerSecond) ? 'correct' : 'incorrect'}\n'
-      'Second part solution: $secondSolution\n');
+      'which is ${_isCorrect(secondExampleSolution, exampleAnswerSecond) ? 'correct' : 'incorrect'} (${secondExampleTime}ms)\n'
+      'Second part solution: $secondSolution (${secondSolutionTime}ms)\n');
 }
 
 late Object Function(List<String> dataLines) calculateFirst;
