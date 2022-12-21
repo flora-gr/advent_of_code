b4251e99dddea01e9350a7e0e60d7e409a9e2c5c
Flora Groothuizen <floragroothuizen@gmail.com>
2022-12-21 20:29:17 +0100

    Minor adjustments

--- lib/day21.dart
+++ lib/day21.dart
@@ -3,71 +3,56 @@ import 'package:rational/rational.dart';
 import 'base/base.dart' as base;
 
 Future<void> calculate() async {
-  base.calculateFirst = _getRootMonkeyValue;
-  base.calculateSecond = _getMyValue;
+  base.calculateFirst = _getRootMonkeyNumber;
+  base.calculateSecond = _getMyNumber;
   base.exampleAnswerFirst = 152;
   base.exampleAnswerSecond = 301;
 
   await base.calculate(21);
 }
 
-int _getRootMonkeyValue(List<String> dataLines) {
+const String root = 'root';
+
+int _getRootMonkeyNumber(List<String> dataLines) {
   final List<Monkey> monkeys = _getMonkeys(dataLines);
 
-  while (_getMonkeyNumber(monkeys, 'root') == null) {
-    for (Monkey monkey
-        in monkeys.where((Monkey monkey) => monkey.number == null)) {
-      final Rational? first =
-          _getMonkeyNumber(monkeys, monkey.operation!.first);
-      final Rational? second =
-          _getMonkeyNumber(monkeys, monkey.operation!.last);
-      if (first != null && second != null) {
-        monkey.number = _performOperation(monkey.operation![1], first, second);
-      }
-    }
+  while (_getMonkeyNumber(monkeys, root) == null) {
+    _doMonkeyGame(monkeys);
   }
 
-  return _getMonkeyNumber(monkeys, 'root')!.toBigInt().toInt();
+  return _getMonkeyNumber(monkeys, root)!.toBigInt().toInt();
 }
 
-int _getMyValue(List<String> dataLines) {
+int _getMyNumber(List<String> dataLines) {
   const String me = 'humn';
 
+  // Try shouting zero to get addition part
   List<Monkey> monkeys = _getMonkeys(dataLines);
   List<String> rootOperation =
-      monkeys.singleWhere((Monkey monkey) => monkey.name == 'root').operation!;
-
+      monkeys.singleWhere((Monkey monkey) => monkey.name == root).operation!;
   monkeys.singleWhere((Monkey monkey) => monkey.name == me).number =
       0.toRational();
-  _tryMonkeyGame(monkeys, rootOperation);
+  while (_getMonkeyNumber(monkeys, rootOperation.first) == null ||
+      _getMonkeyNumber(monkeys, rootOperation.last) == null) {
+    _doMonkeyGame(monkeys);
+  }
   final Rational leftAddition = _getMonkeyNumber(monkeys, rootOperation.first)!;
   final Rational target = _getMonkeyNumber(monkeys, rootOperation.last)!;
 
+  // Try shouting one to get multiplication part
   monkeys = _getMonkeys(dataLines);
   monkeys.singleWhere((Monkey monkey) => monkey.name == me).number =
       1.toRational();
-  _tryMonkeyGame(monkeys, rootOperation);
-  final Rational leftIfMeOne = _getMonkeyNumber(monkeys, rootOperation.first)!;
-  final Rational leftMultiply = leftIfMeOne - leftAddition;
-
-  final Rational answer = (target - leftAddition) / (leftMultiply);
-  return answer.toBigInt().toInt();
-}
-
-void _tryMonkeyGame(List<Monkey> monkeys, List<String> rootOperation) {
   while (_getMonkeyNumber(monkeys, rootOperation.first) == null ||
       _getMonkeyNumber(monkeys, rootOperation.last) == null) {
-    for (Monkey monkey
-        in monkeys.where((Monkey monkey) => monkey.number == null)) {
-      final Rational? first =
-          _getMonkeyNumber(monkeys, monkey.operation!.first);
-      final Rational? second =
-          _getMonkeyNumber(monkeys, monkey.operation!.last);
-      if (first != null && second != null) {
-        monkey.number = _performOperation(monkey.operation![1], first, second);
-      }
-    }
+    _doMonkeyGame(monkeys);
   }
+  final Rational leftIfMyNumberIsOne =
+      _getMonkeyNumber(monkeys, rootOperation.first)!;
+  final Rational leftMultiplication = leftIfMyNumberIsOne - leftAddition;
+
+  final Rational answer = (target - leftAddition) / (leftMultiplication);
+  return answer.toBigInt().toInt();
 }
 
 List<Monkey> _getMonkeys(List<String> dataLines) {
@@ -80,7 +65,8 @@ List<Monkey> _getMonkeys(List<String> dataLines) {
       number: numberOrOperation.length == 2
           ? Rational.parse(numberOrOperation.last)
           : null,
-      operation: numberOrOperation.sublist(1),
+      operation:
+          numberOrOperation.length == 4 ? numberOrOperation.sublist(1) : null,
     ));
   }
   return monkeys;
@@ -88,6 +74,17 @@ List<Monkey> _getMonkeys(List<String> dataLines) {
 
 Rational? _getMonkeyNumber(List<Monkey> monkeys, String name) {
   return monkeys.singleWhere((Monkey monkey) => monkey.name == name).number;
+}
+
+void _doMonkeyGame(List<Monkey> monkeys) {
+  for (Monkey monkey
+      in monkeys.where((Monkey monkey) => monkey.number == null)) {
+    final Rational? first = _getMonkeyNumber(monkeys, monkey.operation!.first);
+    final Rational? second = _getMonkeyNumber(monkeys, monkey.operation!.last);
+    if (first != null && second != null) {
+      monkey.number = _performOperation(monkey.operation![1], first, second);
+    }
+  }
 }
 
 Rational _performOperation(String operation, Rational first, Rational second) {
